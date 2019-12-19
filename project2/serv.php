02180eae1ed4483b59f07d31367771556fc4e613
abdelrahim123 <48460743+abdelrahim123@users.noreply.github.com>
2019-12-19 02:48:27 +0200

    w

--- project2/serv.php
+++ project2/serv.php
@@ -1,20 +1,20 @@
 <?php
     $con=mysqli_connect("localhost","root","","eventdb");
-    if(isset($_POST['obj']){
+    if(isset($_POST['obj'])){
         $per=json_decode($_POST['obj1'],true);
         if($per!=null){
             $mx="INSERT INTO objectevents(type) VALUES ('$per')";
             $mx=mysqli_query($con,$mx);
         }
     }
-    if(isset($_GET['obj1']){
+    if(isset($_GET['obj1'])){
         $mx="SELECT * FROM objectevents";
         $mx=mysqli_query($con,$mx);
         if($mx->num_rows>0){
             $storedData="";
             while($row=$mx->fetch_assoc()){
                 if($row['type']!=null)
-                    $storedData.=$row['type'].;
+                    $storedData.=$row['type'];
             }
             echo json_encode($storedData);
         }
