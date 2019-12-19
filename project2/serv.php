bc0405365b10b340672c99f5298bbc4ebff1c91f
abdelrahim123 <48460743+abdelrahim123@users.noreply.github.com>
2019-12-19 02:36:40 +0200

    c

--- project2/serv.php
+++ project2/serv.php
@@ -1,5 +1,24 @@
 <?php
-$_POST = json_decode(file_get_contents('php://input'),true);
-
-print_r($_POST);
-  ?>
+    $con=mysqli_connect("localhost","root","","eventdb");
+    if(isset($_POST['obj']){
+        $per=json_decode($_POST['obj1'],true);
+        if($per!=null){
+            $mx="INSERT INTO objectevents(type) VALUES ('$per')";
+            $mx=mysqli_query($con,$mx);
+        }
+    }
+    if(isset($_GET['obj1']){
+        $mx="SELECT * FROM objectevents";
+        $mx=mysqli_query($con,$mx);
+        if($mx->num_rows>0){
+            $storedData="";
+            while($row=$mx->fetch_assoc()){
+                if($row['type']!=null)
+                    $storedData.=$row['type'].;
+            }
+            echo json_encode($storedData);
+        }
+        else
+            echo "No data to retgrieve";
+    }
+?>
